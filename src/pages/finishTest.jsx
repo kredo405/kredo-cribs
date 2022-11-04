c977a76d82756b1960c155f9b5a6a0f177608cac
Kredo <kredik305@gmail.com>
2022-11-05 00:15:33 +0300

    fix get functions

--- src/pages/finishTest.jsx
+++ src/pages/finishTest.jsx
@@ -18,36 +18,36 @@ const FinishTest = () => {
     return (
         <>
             <div className="flex h-80">
-            <Fireworks
-                options={{
-                    rocketsPoint: {
-                        min: 0,
-                        max: 100
-                    }
-                }}
-                style={{
-                    top: 0,
-                    left: 0,
-                    width: '100%',
-                    height: '100%',
-                    position: 'fixed',
-                    background: '#fff',
-                    opacity: 0.4
-                }}
-            />
+                <Fireworks
+                    options={{
+                        rocketsPoint: {
+                            min: 0,
+                            max: 100
+                        },
+                    }}
+                    style={{
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                        position: 'fixed',
+                        background: '#fff',
+                        opacity: 0.3
+                    }}
+                />
             </div>
 
-           <div className="flex flex-col justify-center h-80 px-3">
-           <div className="flex justify-center mb-5">
-                <h1 className="font-mono text-4xl font-bold text-sky-900">Тест завершён</h1>
+            <div className="flex flex-col justify-center h-80 px-3">
+                <div className="flex justify-center mb-5">
+                    <h1 className="font-mono text-4xl font-bold text-sky-900">Тест завершён</h1>
+                </div>
+                <div className="flex justify-center">
+                    <p className="text-center font-mono text-xl font-medium text-amber-800">Вы ответили правильно на {state.finishTest.isTrue} из {state.finishTest.questions} вопросов</p>
+                </div>
+                <div className="flex justify-center">
+                    <img src="https://abrakadabra.fun/uploads/posts/2021-12/1639922537_1-abrakadabra-fun-p-risunok-chelovechka-dlya-prezentatsii-1.jpg" alt="people" />
+                </div>
             </div>
-            <div className="flex justify-center">
-                <p className="text-center font-mono text-xl font-medium text-amber-800">Вы ответили правильно на {state.finishTest.isTrue} из {state.finishTest.questions} вопросов</p>
-            </div>
-            <div className="flex justify-center">
-                <img src="https://abrakadabra.fun/uploads/posts/2021-12/1639922537_1-abrakadabra-fun-p-risunok-chelovechka-dlya-prezentatsii-1.jpg" alt="people" />
-            </div>
-           </div>
         </>
     )
 }
--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDoc, doc } from "firebase/firestore";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from 'react';
@@ -12,80 +12,65 @@ const Test = () => {
     const state = useSelector(state => state)
     const history = useNavigate();
     const db = state.db
-    const [data, setData] = useState('gsg')
+    const [data, setData] = useState([{
+        answer: [{ answer: '', isTrue: false }],
+        question: ''
+    }])
     const [value, setValue] = useState('');
     const [correctAnswer, setCorrectAnswer] = useState('');
     const [question, setQuestion] = useState(0);
     const [isTrue, setIsTrue] = useState(0);
 
+    useEffect(() => {
+        const getData = async () => {
+            const citiesRef = doc(db, "mathematics", 'GV4ViLPsYodId9mBBnwv');
+            const docSnap = await getDoc(citiesRef);
 
-    const arr = [
-        {
-            question: '1. В какой паре чисел первое число является делителем второго?',
-            answer: [
-                { isTrue: false, answer: '8 и 10' },
-                { isTrue: false, answer: '7 и 10' },
-                { isTrue: true, answer: '9 и 10' },
-                { isTrue: false, answer: '6 и 10' },
-            ]
-        },
-        {
-            question: '1. В какойdsgdsgsdgsd dsg g sdg dsg sd d s ?',
-            answer: [
-                { isTrue: true, answer: '8 и 10sdg s' },
-                { isTrue: false, answer: '7 и 1sdg ds0' },
-                { isTrue: false, answer: '9 sdg dsg и 10' },
-                { isTrue: false, answer: '6 sdssdgи 10' },
-            ]
-        },
-        {
-            question: '1. В кsd gsdак ойd sdg  sgds gsdgsdgsdgsdg d sg g sdg dsg sd d s ?',
-            answer: [
-                { isTrue: false, answer: '8 и 10sdg s' },
-                { isTrue: false, answer: '7sd g  sdgи 1sdg ds0' },
-                { isTrue: false, answer: '9 s dgsdg sdg dsg и 10' },
-                { isTrue: true, answer: '6 s dgsdssdgи 10' },
-            ]
-        },
-    ]
+            console.log(docSnap.data())
+
+            setData(docSnap.data().howClass.sixClass.tests[0].questions)
+
+        }
+
+        getData()
+    }, [])
+
 
     const onChange = (e) => {
-        console.log(e.target.value);
+        console.log(e)
+
         setValue(e.target.value);
-        arr[question].answer.forEach(el => {
-            if (el.isTrue) {
-                setCorrectAnswer(el.answer)
-            }
+        data[question].answer.forEach(el => {
+                if (el.isTrue) {
+                    setCorrectAnswer(el.answer)
+                }
         })
+
+
     };
 
     const onSubmit = (e) => {
         e.preventDefault()
-
         if (value === correctAnswer) {
             dispatch({
                 type: 'FINISHTEST',
                 payload: {
-                    questions: arrMap.length,
+                    questions: data.length,
                     isTrue: isTrue + 1,
                 }
             })
-            
+
             setIsTrue(isTrue + 1)
         }
-        
-        if (arrMap.length > question + 1) {
+
+        if (data.length > question + 1) {
             setQuestion(question + 1)
         } else {
             history('/finishTest')
         }
-
-       
     }
 
-    console.log(`Верно ${isTrue}`)
-
-    const arrMap = arr.map((el, index) => {
+    const arrMap = data.map((el, index) => {
         const radioButtons = el.answer.map((Item, i) => {
             return (
                 <div className='flex items-center py-3' key={i}>
@@ -101,7 +86,6 @@ const Test = () => {
                 </div>
                 <form
                     onSubmit={onSubmit}
-                    required
                     className="px-3 w-full mt-3"
                 >
                     <div className="flex flex-col items-start">
@@ -119,19 +103,6 @@ const Test = () => {
             </div>
         )
     })
-
-
-
-    useEffect(() => {
-        const getData = async () => {
-            const querySnapshot = await getDocs(collection(db, "mathematics"));
-            querySnapshot.forEach((doc) => {
-                console.log(doc.data());
-            });
-        }
-
-        getData()
-    }, [])
 
 
     return (
